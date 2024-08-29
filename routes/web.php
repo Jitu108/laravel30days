88c4e20d947f4fcc08acb0464969cfbca4304ec4
Jitendra Gupta <gupta.jitendra108@gmail.com>
2024-08-29 14:55:35 +0530

    Worked on basic laravel

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+    // return 'About Page';
+    // return ['foo' => 'bar'];
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
